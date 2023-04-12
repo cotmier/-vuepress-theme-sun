d4ee1e3b4bf2df4080e617d5b31a725f40d6e469
cotmier <cotmier@outlook.com>
2023-04-12 15:21:40 +0000

    update hello.java. HttpServlet
    Signed-off-by: cotmier <cotmier@outlook.com>

--- hello.java
+++ hello.java
@@ -1,3 +1,9 @@
-main(){
-    system.out.pintln();
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //处理 GET 请求
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //处理 POST 请求
+    }
 }
